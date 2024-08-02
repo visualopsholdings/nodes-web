095b9bf4423e176114677ee2ae1c05b42100d98e
Paul Hamilton <phamtec@mac.com>
2024-08-02 17:13:11 +1000

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,10 +4,15 @@
 
 if [ ! -f "nodes-web/build/nodes-web" ];
 then
-  echo "Need to build project first"
+  echo "Need to build or install project first"
   exit 1
 fi
 
-./nodes-web/build/nodes-web \
+if [ -d /home/nodes/nodes-lib ];
+then
+  $LIBS="LD_LIBRARY_PATH=/usr/local/lib:/home/nodes/nodes/build:/home/nodes/nodes-lib"
+fi
+
+$LIBS ./nodes-web/build/nodes-web \
   --logLevel=trace \
   > nodes-web.log 2>&1 &
