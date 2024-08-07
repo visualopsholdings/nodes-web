0eea7a46a487303a4430cc1f8af9ec4f40a0f4fb
Paul Hamilton <phamtec@mac.com>
2024-08-07 16:43:41 +1000

    Add commented out service for running against real visualops.

--- config/services.rb
+++ config/services.rb
@@ -1,5 +1,12 @@
 require 'service_manager'
 
+# ServiceManager.define_service 'vops' do |s|
+#    s.start_cmd = 'cd $VOPS_HOME; MONGO_PORT_27017_TCP_ADDR=127.0.0.1 MONGO_PORT_27017_TCP_PORT=27017 node test-app.js -p 3000 --memcached'
+#    s.loaded_cue = /start_tests/
+#    s.cwd = Dir.pwd
+#    s.pid_file = 'vops.pid'
+# end
+
 ServiceManager.define_service 'nodes' do |s|
    s.start_cmd = '$NODES_HOME/build/nodes --test --logLevel=trace'
    s.loaded_cue = /Local REP/
