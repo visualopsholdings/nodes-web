bb19aaa5b800702402f23f62413fdb05a90f0430
Paul Hamilton <phamtec@mac.com>
2024-11-12 16:33:38 +1100

    Update nodes-web.cpp

--- src/nodes-web.cpp
+++ src/nodes-web.cpp
@@ -27,7 +27,7 @@ using json = boost::json::value;
 
 int main(int argc, char *argv[]) {
 
-  string version = "Nodes WEB v0.4.1, 8-Oct-2024.";
+  string version = "Nodes WEB v0.5.0, 12-Nov-2024.";
 
   int httpPort;
   int reqPort;
